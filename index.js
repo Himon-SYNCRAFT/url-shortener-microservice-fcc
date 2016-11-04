93fa71850d3a155954028aa19bf2158f3037b27e
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-11-04 23:21:34 +0100

    Example Usage on index page

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const connectionString = 'mongodb://anon:anon@ds143777.mlab.com:43777/short_urls
 const port = process.env.PORT || 3000
 
 let app = express()
+app.set('view engine', 'pug')
 
 function isValidUrl(url) {
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)
 }
 
 app.get('/', (request, response) => {
-    response.send('hello')
+    response.render('index')
 })
 
 app.get('/add/*', (request, response) => {
